0717874623f0d0a05f3eb4e5a929492755f6c100
Andy Pak <im.andy.pak@gmail.com>
2024-11-20 00:46:30 -0500

    Create function headers for each database interaction

--- database.h
+++ database.h
@@ -3,3 +3,9 @@ void close_connection(sqlite3 *db);
 
 // init database return db else 0 cuz not work
 int initialize_database(sqlite3 *db);
+
+int add_password(sqlite3 *db, char *name, char *password);
+int get_password(sqlite3 *db, char *name);
+int update_password(sqlite3 *db, char *name, char *old_password, char *new_password);
+int delete_password(sqlite3 *db, char *name);
+int list_passwords(sqlite3 *db);
