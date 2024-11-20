db6270322db7bf0a2280728949dec096d2493424
Andy Pak <im.andy.pak@gmail.com>
2024-11-20 01:18:27 -0500

    Create check status and check is done functions

--- database.c
+++ database.c
@@ -2,10 +2,25 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
+int check_status(int status, sqlite3 *db) {
+    if (status != SQLITE_OK) {
+        printf("error: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+    return 1;
+}
+
+int check_is_done(int status, sqlite3 *db) {
+    if (status != SQLITE_DONE) {
+        printf("error: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+    return 1;
+}
+
 int open_connection(sqlite3 **db, char *database_path) {
     int status = sqlite3_open(database_path, db);
-    if (status != SQLITE_OK) {
-        printf("error: %s\n", sqlite3_errmsg(*db));
+    if (!check_status(status, *db)) {
         return 0;
     }
     return 1;
@@ -42,8 +57,7 @@ int add_password(sqlite3 *db, char *name, char *password) {
     char *sql = "INSERT INTO passwords (name, password) VALUES (?, ?);";
 
     int status = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    if (status != SQLITE_OK) {
-        printf("error: %s\n", sqlite3_errmsg(db));
+    if (!check_status(status, db)) {
         return 0;
     }
 
@@ -52,8 +66,7 @@ int add_password(sqlite3 *db, char *name, char *password) {
     sqlite3_bind_text(stmt, 2, password, -1, SQLITE_STATIC);
 
     status = sqlite3_step(stmt);
-    if (status != SQLITE_DONE) {
-        printf("error: %s\n", sqlite3_errmsg(db));
+    if (!check_is_done(status, db)) {
         return 0;
     }
     return 1;
--- database.h
+++ database.h
@@ -1,9 +1,15 @@
+// Check status codes functions
+int check_status(int status, sqlite3 *db);
+int check_is_done(int status, sqlite3 *db);
+
+// Open and close the connection
 int open_connection(sqlite3 **db, char *database_path);
 void close_connection(sqlite3 *db);
 
-// init database return db else 0 cuz not work
+// Initialize the database
 int initialize_database(sqlite3 *db);
 
+// Interactions with database
 int add_password(sqlite3 *db, char *name, char *password);
 int get_password(sqlite3 *db, char *name);
 int update_password(sqlite3 *db, char *name, char *old_password, char *new_password);
